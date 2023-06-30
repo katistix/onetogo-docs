490b93300c8d36c71cc9150d28d9fd3a684da675
Paul <2018.tpg@gmail.com>
2023-06-30 19:40:18 +0300

    initial

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+    logo: <span>OneToGo Docs</span>,
+    project: {
+        link: "https://github.com/katistix/onetogo",
+    },
+    footer: {
+        text: "Paul Tal @katistix 2023",
+    },
+    useNextSeoProps() {
+        return {
+            titleTemplate: "%s - OneToGo Docs",
+        };
+    },
+};
 
-export default config
+export default config;
